0d141ef06b101da8031bf1ba9594cee5dc3e6def
Artyom Kapustin <shmuritto@gmail.com>
2023-04-04 18:04:28 +0600

    init

---
+++ src/ArrayPrettyPrinter.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class ArrayPrettyPrinter {
+
+    public static void main(String[] args) {
+        System.out.println(formatArray(new String[]{"", ""}));
+    }
+
+    /**
+     * <pre>
+     * Превратить массив символов в строку со следущеми требованиями:
+     * 1. Первая буква в строке всегда заглавная
+     * 2. Элементы массива должны быть разделены проблами
+     * 3. Все остальные символы за первым, должны быть строчными
+     * 4. В конце должен быть восклицательный знак
+     * 5. Если массив пустой или null, то должна быть возвращена пустая строка.
+     * 6. Если один из элементов массив null, то его нужно вывести как null, правило 1. должно действовать
+     * 7. Если один из элементов массива пустой, то его выводим как empty, правило 1. должно действовать
+     *
+     * Примеры:
+     * ["hellow", "my friend] = "Hellow my friend!"
+     * ["hellow] = "Hellow!"
+     * ["Hi", "Mari"] = "Hi mari!"
+     * [null", "Mari"] = "Null mari!"
+     * [null", "null"] = "Null null!"
+     * ["", "Mari"] = "Empty mari!"
+     * ["", ""] = "Empty empty!"
+     * null --> ""
+     * [] ---> ""
+     * </pre>
+     *
+     * @param strArray массив для форматирования
+     * @return результат форматирования
+     */
+    private static String formatArray(String[] strArray) {
+        String string = "";
+        if (strArray.length == 0 || Arrays.toString(strArray).equals("")) {
+            return string;
+        } else {
+            for (int i = 0; i < strArray.length; i++) {
+                if (strArray[i] == null) {
+                    strArray[i] = "null";
+                }
+                if (strArray[i].equals("")) {
+                    strArray[i] = "empty";
+                }
+                if (i != 0) {
+                    string = string + " " + strArray[i];
+                } else {
+                    string = string + strArray[i];
+                }
+                if (i == strArray.length - 1) {
+                    string = string + "!";
+                }
+            }
+
+            string = string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+
+        }
+        return string;
+    }
+}
